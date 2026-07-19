e6f5b28e53ebe3b401d94efbc065bb12cbb9bb5f
dsabogals <dsabogals@unal.edu.co>
2026-07-19 02:14:18 -0500

    relee_actu

--- relee_controller.v
+++ relee_controller.v
@@ -1,11 +1,11 @@
 module relee_controller (
-    input kids,
+    input kids, password,
     input time_finish,
     output reg off_enable
 );
 
 always @(*) begin
-    if (kids && time_finish) begin
+    if ((kids && time_finish) || password) begin
         off_enable = 1'b1;
     end else begin
         off_enable = 1'b0;
